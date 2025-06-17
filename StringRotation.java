33ec95e55f85991bc8d59502b25520bcd3a1b00c
sivagangadharthecoder <22A91A1298@aec.edu.in>
2025-06-17 10:36:06 +0530

    string rotation

---
+++ StringRotation.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class StringRotation {
+    private static String originalString;
+    private static String rotatedString;
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string:");
+        originalString = scanner.nextLine();
+        System.out.print("Enter the second string:");
+        rotatedString = scanner.nextLine();
+
+        if (isRotated(originalString, rotatedString)) {
+            System.out.println("Yes. The second string is a rotation of the first string.");
+        } else {
+            System.out.println("No. The second string is NOT a rotation of the first string.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isRotated(String original, String rotated) {
+        if (original.length() != rotated.length()) {
+            return false;
+        }
+        String concatenated = rotated + rotated;
+        return concatenated.contains(original);
+    }
+}
