628ad2e84df863d01f2ada5d1adf506b23fa4cb5
sivagangadharthecoder <22A91A1298@aec.edu.in>
2025-07-05 12:04:56 +0530

    fghj

---
+++ UnorderdedIncreasing.java
@@ -0,0 +1,22 @@
+public class UnorderdedIncreasing {
+    public static void main(String[] args) {
+        int[] arr = { 10, 9, 2, 5, 3, 7 };
+        System.out.println("Length of longest unordered increasing subsequence is: " + findUnorderedIncreasing(arr));
+    }
+
+    private static int findUnorderedIncreasing(int[] arr) {
+        int maxLen = 1;
+
+        int[] dp = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+        return maxLen;
+    }
+}
