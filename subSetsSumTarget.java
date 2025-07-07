3cd17e826d37058b859935ff7cc6463f9b399a90
sivagangadharthecoder <22A91A1298@aec.edu.in>
2025-07-07 14:50:05 +0530

    subSetsSumTarget

--- subSetsSumTarget.java
+++ subSetsSumTarget.java
@@ -1,27 +1,27 @@
 public class subSetsSumTarget {
-    public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5 };
-        int target = 9;
-        int count = countSubsets(arr, target);
-        System.out.println("Number of subsets: " + count);
-    }
+  public static void main(String[] args) {
 
-    public static int countSubsets(int[] arr, int target) {
-        int n = arr.length;
-        int totalSubsets = 1 << n;
-        int count = 0;
+    int[] arr = { 1, 2, 3, 4, 5, 6 };
+    int target = 7;
 
-        for (int mask = 0; mask < totalSubsets; mask++) {
-            int currentSum = 0;
-            for (int i = 0; i < n; i++) {
-                if ((mask & (1 << i)) != 0) {
-                    currentSum += arr[i];
-                }
-            }
-            if (currentSum == target) {
-                count++;
-            }
-        }
-        return count;
+    System.out.print("total subsets: " + countSubSets(arr, target));
+  }
+
+  private static int countSubSets(int[] arr, int target) {
+    int n = arr.length;
+    int totalSubSets = 1 << n;
+    int count = 0;
+
+    for (int i = 0; i < totalSubSets; i++) {
+      int currSum = 0;
+      for (int j = 0; j < n; j++) {
+        if ((i & (1 << j)) != 0)
+          currSum += arr[j];
+      }
+      if (currSum == target)
+        count++;
     }
+    return count;
+  }
+
 }
