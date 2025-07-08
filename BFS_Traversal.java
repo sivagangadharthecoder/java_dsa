2762216d51225d28c93c4c49c04c1586356cfc53
sivagangadharthecoder <22A91A1298@aec.edu.in>
2025-07-08 10:11:14 +0530

    bfs

---
+++ BFS_Traversal.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class BFS_Traversal {
+    public ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (adj == null || adj.size() == 0)
+            return result;
+
+        int V = adj.size();
+        boolean[] visited = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                Queue<Integer> queue = new LinkedList<>();
+                queue.add(i);
+                visited[i] = true;
+
+                while (!queue.isEmpty()) {
+                    int current = queue.poll();
+                    result.add(current);
+
+                    for (int neighbor : adj.get(current)) {
+                        if (!visited[neighbor]) {
+                            visited[neighbor] = true;
+                            queue.add(neighbor);
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        BFS_Traversal solution = new BFS_Traversal();
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        adj.add(new ArrayList<>(Arrays.asList(1, 3)));
+        adj.add(new ArrayList<>(Arrays.asList(0, 2, 4)));
+        adj.add(new ArrayList<>(Arrays.asList(1)));
+        adj.add(new ArrayList<>(Arrays.asList(0, 4)));
+        adj.add(new ArrayList<>(Arrays.asList(1, 3)));
+
+        ArrayList<Integer> bfsTraversal = solution.bfs(adj);
+
+        System.out.println("BFS Traversal: " + bfsTraversal);
+    }
+}
