b745b5aa76b33ef3db6c6f76da3dcc1d99cb1d38
sivagangadharthecoder <22A91A1298@aec.edu.in>
2025-07-18 11:02:00 +0530

    Interface Example

---
+++ InterfaceEx.java
@@ -0,0 +1,29 @@
+interface Animal {
+    void eat();
+
+    void sleep();
+
+    int val1 = 120;
+    int val2 = 120;
+}
+
+class Dog implements Animal {
+    public void eat() {
+        System.out.println("Dog is eating");
+        int val1 = 100; // local variable has higher preference
+        System.out.println(val1);
+        System.out.println(val2);
+    }
+
+    public void sleep() {
+        System.out.println("Dog is sleeping");
+    }
+}
+
+class InterfaceEx {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.sleep();
+    }
+}
